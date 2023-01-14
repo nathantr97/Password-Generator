4239c55e080ddd1bf23afd77356a7cc7d6a061f8
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-01-14 09:35:51 -0800

    added function in js and return to get user inputs

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// added fucntion to generate a password with the use of boolean to 
+// determine values are strictly true
+
+function generatePassword() {
+  selectLength();
+  if (acceptData ===true) {
+    if (getUserInput()===false) {
+      return;
+}
+passRandomizer();
+      return;
+} else {
+    console.log(acceptData)
+    return;
+}
+}
+
